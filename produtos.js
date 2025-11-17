c2de5334d49f244d4f62c6a5f66bf243393832f1
GaleriasJ3Ms <i.s.m@outlook.com>
2025-11-17 15:08:11 +0100

    Add files via upload

--- produtos.js
+++ produtos.js
@@ -10,8 +10,32 @@ fetch('produtos.json')
         <h3>${p.nome}</h3>
         <p>${p.descricao}</p>
         <p class="preço">${p.preco}€</p>
+        <button class="add-to-cart" data-id="${p.id}" data-nome="${p.nome}" data-preco="${p.preco}" data-imagem="${p.imagem}">
+            🛒
+        </button>
       `;
       container.appendChild(div);
     });
+    iniciarCarrinho();
   })
   .catch(error => console.error('Erro ao carregar produtos:', error));
+
+  function iniciarCarrinho() {
+  let carrinho = JSON.parse(localStorage.getItem("carrinho")) || [];
+
+  document.querySelectorAll(".add-to-cart").forEach(botao => {
+    botao.addEventListener("click", () => {
+      const item = {
+        id: botao.dataset.id,
+        nome: botao.dataset.nome,
+        preco: botao.dataset.preco,
+        imagem: botao.dataset.imagem
+      };
+
+      carrinho.push(item);
+      localStorage.setItem("carrinho", JSON.stringify(carrinho));
+
+      alert(item.nome + " foi adicionado ao carrinho!");
+    });
+  });
+}
